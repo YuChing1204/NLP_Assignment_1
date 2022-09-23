e27353d1607fc0f83b89324037cb577c299ee019
rlwPosits <southpaw.rlw@gmail.com>
2022-09-22 22:06:20 -0500

    test data predictions implemented

--- SpamClassifier.py
+++ SpamClassifier.py
@@ -1,13 +1,34 @@
 import math
+import nltk
 from nltk.util import ngrams
 from nltk.corpus import stopwords
+import string
 
 # each line is a list element
 def read_from_file(filepath):
+    raw_data = []
     with open(filepath, "r") as f:
-        raw_data = f.readlines()
+        for line in f:
+            raw_data.append(line.strip())
     return raw_data
 
+# each line is a list element
+def labels_from_file(filepath):
+    raw_data = []
+    with open(filepath, "r") as f:
+        for line in f:
+            raw_data.append(int(line.strip()))
+    return raw_data
+
+
+def preprocess_text(line):
+    tokenizer = nltk.RegexpTokenizer(r"\w+")
+    words = tokenizer.tokenize(line.lower())
+    stops = set(stopwords.words('english'))
+    tokens = [word for word in words if word not in stops]
+    tokens.insert(0, '<s>')
+    tokens.append('</s>')
+    return tokens
 
 # NGRAMS **********************************************************
 # processes raw data and return unigrams dictionary
@@ -15,14 +36,12 @@ def read_from_file(filepath):
 def get_unigrams(raw_data):
     unigrams_dict = {}
     for text in raw_data:
-        updated = '<s> ' + text + ' </s>'
-        tokens = updated.split()
+        tokens = preprocess_text(text)
         for token in set(tokens):
-            temp = token.lower()
-            if temp in unigrams_dict:
-                unigrams_dict[temp] += tokens.count(temp)
+            if token in unigrams_dict:
+                unigrams_dict[token] += tokens.count(token)
             else:
-                unigrams_dict[temp] = tokens.count(temp)
+                unigrams_dict[token] = tokens.count(token)
     return unigrams_dict
 
 
@@ -31,16 +50,12 @@ def get_unigrams(raw_data):
 def get_ngrams(raw_data, n):
     ngrams_dict = {}
     for text in raw_data:
-        ngrams_list = list(ngrams(text.split(), n, pad_left=True,
-                                                   left_pad_symbol='<s>',
-                                                   pad_right=True,
-                                                   right_pad_symbol='</s>'))
+        ngrams_list = list(ngrams(preprocess_text(text), n))
         for n_gram in ngrams_list:
-            temp = (n_gram[0].lower(), n_gram[1].lower())
-            if temp in ngrams_dict:
-                ngrams_dict[temp] += ngrams_list.count(temp)
+            if n_gram in ngrams_dict:
+                ngrams_dict[n_gram] += ngrams_list.count(n_gram)
             else:
-                ngrams_dict[temp] = ngrams_list.count(temp)
+                ngrams_dict[n_gram] = ngrams_list.count(n_gram)
     return ngrams_dict
 
 
@@ -73,8 +88,7 @@ def smoothed_bigram_probabilities(unigrams_dict, bigrams_dict, k):
 # PERPLEXITY ******************************************************
 def get_ngram_perplexity(record, trained_probabilities, n):
     summation = 0
-    temp = '<s> ' + record + ' </s>'
-    words = temp.lower().split()
+    words = preprocess_text(record)
     M = len(words)
     ngram_list = list(ngrams(words, n)) if n > 1 else words
     unk_key = '<UNK>' if n == 1 else ('<UNK>', '<UNK>')
@@ -93,10 +107,18 @@ def make_predictions(test_data, truthful_model, deceptive_model, n):
         truth_score = get_ngram_perplexity(line, truthful_model, n)
         deceptive_score = get_ngram_perplexity(line, deceptive_model, n)
         if truth_score < deceptive_score:
-            predictions.append('T')
+            predictions.append(0)
         else:
-            predictions.append('D')
+            predictions.append(1)
     return predictions
+
+
+def check_accuracy(predictions, actuals):
+    accurate = 0
+    for predicted, actual in zip(predictions, actuals):
+        if predicted == actual:
+            accurate += 1
+    return accurate / len(predictions) * 100.0
 
 
 if __name__ == "__main__":
@@ -112,18 +134,28 @@ if __name__ == "__main__":
     de_unigrams_dict = get_unigrams(de_train_data)
     de_bigrams_dict = get_ngrams(de_train_data, 2)
 
+
     # UNKNOWN WORD HANDLING
-    updated_tr_unigrams = get_unknown_ngrams(tr_unigrams_dict, 1, 3)
-    updated_tr_bigrams = get_unknown_ngrams(tr_bigrams_dict, 2, 3)
-    updated_de_unigrams = get_unknown_ngrams(de_unigrams_dict, 1, 3)
-    updated_de_bigrams = get_unknown_ngrams(de_bigrams_dict, 2, 3)    
+    updated_tr_unigrams = get_unknown_ngrams(tr_unigrams_dict, 1, 35)
+    updated_tr_bigrams = get_unknown_ngrams(tr_bigrams_dict, 2, 0)
+    updated_de_unigrams = get_unknown_ngrams(de_unigrams_dict, 1, 35)
+    updated_de_bigrams = get_unknown_ngrams(de_bigrams_dict, 2, 0)
+    # print('Truthful unigram UNK count:  ' + str(updated_tr_unigrams['<UNK>']))
+    # print('Truthful bigram UNK count:   ' + str(updated_tr_bigrams[('<UNK>', '<UNK>')]))
+    # print('Deceptive unigram UNK count: ' + str(updated_de_unigrams['<UNK>']))
+    # print('Deceptive bigram UNK count:  ' + str(updated_de_bigrams[('<UNK>', '<UNK>')])) 
     
+
     # SMOOTHING
     smoothed_tu = smoothed_unigram_probabilities(updated_tr_unigrams, 1)
     smoothed_tb = smoothed_bigram_probabilities(updated_tr_unigrams, updated_tr_bigrams, 1)
     smoothed_du = smoothed_unigram_probabilities(updated_de_unigrams, 1)
     smoothed_db = smoothed_bigram_probabilities(updated_de_unigrams, updated_de_bigrams, 1)
 
+    # print('Truthful unigram UNK prob:  ' + str(smoothed_tu['<UNK>']))
+    # print('Truthful bigram UNK prob:   ' + str(smoothed_tb[('<UNK>', '<UNK>')]))
+    # print('Deceptive unigram UNK prob: ' + str(smoothed_du['<UNK>']))
+    # print('Deceptive bigram UNK prob:  ' + str(smoothed_db[('<UNK>', '<UNK>')])) 
     # VALIDATION ***************************************************************************
     val_tr_data = read_from_file('A1_DATASET\\validation\\truthful.txt')
     val_de_data = read_from_file('A1_DATASET\\validation\\deceptive.txt')
@@ -133,18 +165,43 @@ if __name__ == "__main__":
     val_tr_bi_preds = make_predictions(val_tr_data, smoothed_tb, smoothed_db, 2)
     val_de_bi_preds = make_predictions(val_de_data, smoothed_tb, smoothed_db, 2)
   
-    print(f'\nVALIDATION TRUTHFUL PREDICTIONS ({len(val_tr_uni_preds)})')
-    tu_tr_count = val_tr_uni_preds.count('T')
-    tu_de_count = val_tr_uni_preds.count('D')
-    print(f'Unigrams:\ttruthful = {tu_tr_count}\tdeceptive = {tu_de_count}')
-    tb_tr_count = val_tr_bi_preds.count('T')
-    tb_de_count = val_tr_bi_preds.count('D')
-    print(f'Bigrams:\ttruthful = {tb_tr_count}\tdeceptive = {tb_de_count}')
+    print(f'\nVALIDATION TRUTHFUL PREDICTIONS ({len(val_tr_uni_preds)}) ******')
+    val_tr_labels = [0] * len(val_tr_uni_preds)
+    tu_tr_count = val_tr_uni_preds.count(0)
+    tu_de_count = val_tr_uni_preds.count(1)
+    print(f'\nUnigrams:\ttruthful = {tu_tr_count}\tdeceptive = {tu_de_count}')
+    print(f'VAL TRUTHFUL UNIGRAM ACCURACY = {check_accuracy(val_tr_uni_preds, val_tr_labels)}')
+    tb_tr_count = val_tr_bi_preds.count(0)
+    tb_de_count = val_tr_bi_preds.count(1)
+    print(f'\nBigrams:\ttruthful = {tb_tr_count}\tdeceptive = {tb_de_count}')
+    print(f'VAL TRUTHFUL BIGRAM ACCURACY = {check_accuracy(val_tr_bi_preds, val_tr_labels)}')
 
-    print(f'\nVALIDATION DECEPTIVE PREDICTIONS ({len(val_de_uni_preds)})')
-    du_tr_count = val_de_uni_preds.count('T')
-    du_de_count = val_de_uni_preds.count('D')
-    print(f'Unigrams:\ttruthful = {du_tr_count}\tdeceptive = {du_de_count}')
-    db_tr_count = val_de_bi_preds.count('T')
-    db_de_count = val_de_bi_preds.count('D')
-    print(f'Bigrams:\ttruthful = {db_tr_count}\tdeceptive = {db_de_count}')
+    print(f'\nVALIDATION DECEPTIVE PREDICTIONS ({len(val_de_uni_preds)}) ******')
+    val_de_labels = [1] * len(val_de_uni_preds)
+    du_tr_count = val_de_uni_preds.count(0)
+    du_de_count = val_de_uni_preds.count(1)
+    print(f'\nUnigrams:\ttruthful = {du_tr_count}\tdeceptive = {du_de_count}')
+    print(f'VAL DECEPTIVE UNIGRAM ACCURACY = {check_accuracy(val_de_uni_preds, val_de_labels)}')
+    db_tr_count = val_de_bi_preds.count(0)
+    db_de_count = val_de_bi_preds.count(1)
+    print(f'\nBigrams:\ttruthful = {db_tr_count}\tdeceptive = {db_de_count}')
+    print(f'VAL DECEPTIVE BIGRAM ACCURACY = {check_accuracy(val_de_bi_preds, val_de_labels)}')
+
+
+# TESTING **********************************************************************
+    test_data = read_from_file('A1_DATASET\\test\\test.txt')
+    test_labels = labels_from_file('A1_DATASET\\test\\test_labels.txt')
+
+    test_uni_preds = make_predictions(test_data, smoothed_tu, smoothed_du, 1)
+    test_bi_preds = make_predictions(test_data, smoothed_tb, smoothed_db, 2)
+  
+    print(f'\nTEST PREDICTIONS ({len(test_uni_preds)}) ******')
+    tu_tr_count = test_uni_preds.count(0)
+    tu_de_count = test_uni_preds.count(1)
+    print(f'Unigrams:\ttruthful = {tu_tr_count}\tdeceptive = {tu_de_count}')
+    tb_tr_count = test_uni_preds.count(0)
+    tb_de_count = test_uni_preds.count(1)
+    print(f'Bigrams:\ttruthful = {tb_tr_count}\tdeceptive = {tb_de_count}')
+    print(f'\nTEST UNIGRAM ACCURACY = {check_accuracy(test_uni_preds, test_labels)}')
+    print(f'TEST BIGRAM ACCURACY = {check_accuracy(test_bi_preds, test_labels)}')
+    
